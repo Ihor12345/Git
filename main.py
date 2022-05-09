2197bea580d63df4c6cf65299f5d915ec8344fae
Ihor213 <105105027+Ihor213@users.noreply.github.com>
2022-05-09 19:08:29 +0300

    cat and dog commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+class cat :
+    def __init__(self, name=None):
+        self.name = name
+    def __del__(self):
+        print("Go eat. The food is already in the bowls.!")
+Tom = cat()
+
+
+class dog:
+    def __init__(self, name=None):
+        self.name = name
+    def __del__(self):
+        print("Go eat. The food is already in the bowls.!")
+Jack = dog()
